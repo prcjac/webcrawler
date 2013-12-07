6c6fa93756328bd39b8e9eebdbd750e7f488ebd4
Peter Cowan <prcjac@yahoo.co.uk>
2013-12-07 13:17:46 +0000

    Added skeleton of main class.

---
+++ src/org/prcjac/webcrawler/WebCrawler.java
@@ -0,0 +1,15 @@
+package org.prcjac.webcrawler;
+
+/**
+ * Main class for the webcrawler.
+ * 
+ * @author peter
+ * 
+ */
+public class WebCrawler {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
