3512507898256e657469eac248412a5402356f32
Peter Cowan <prcjac@yahoo.co.uk>
2013-12-27 19:23:24 +0000

    * Updated the input stream tostring logic as I'm not comfortable with the mysterious scanner yet! * Added gatherer that collects until no new uris have been discovered. Fatally flawed as it doesn't cope for uris that aren't prefixed by the root uri but we'll get to it

--- src/org/prcjac/webcrawler/WebTrawler.java
+++ src/org/prcjac/webcrawler/WebTrawler.java
@@ -1,7 +1,9 @@
 package org.prcjac.webcrawler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -9,7 +11,6 @@ import java.net.URLConnection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 
 import org.jsoup.Jsoup;
@@ -37,8 +38,24 @@ public class WebTrawler {
 
 	public void gather() throws MalformedURLException, IOException {
 		String path = _rootURI.getPath();
-		_discoveredURIs.add(URI.create(path.substring(path.lastIndexOf('/') + 1)));
-
+		URI relativeRootURI = URI.create(path.substring(path.lastIndexOf('/') + 1));
+		_discoveredURIs.add(relativeRootURI);
+		Set<URI> ofInterest = new LinkedHashSet<>();
+		ofInterest.add(relativeRootURI);
+		while (!ofInterest.isEmpty()) {
+			URI first = ofInterest.iterator().next();
+			// Should probably be a debug line instead.
+			System.err.println("Scanning document at " + _rootURIAsString + "/"
+					+ first.toString());
+			// These should all be relative...
+			Set<URI> discovered = discover(first);
+			for (URI discoveredURI : discovered) {
+				if (!_discoveredURIs.contains(discoveredURI)) {
+					_discoveredURIs.add(discoveredURI);
+					ofInterest.add(discoveredURI);
+				}
+			}
+		}
 	}
 
 	private Set<URI> discover(final URI uri) throws MalformedURLException, IOException {
@@ -49,30 +66,33 @@ public class WebTrawler {
 		if (urlConnection instanceof HttpURLConnection) {
 			HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
 			int responseCode = httpURLConnection.getResponseCode();
-			//Handle 301 redirects
+			// Handle 301 redirects
 			if (responseCode == 301) {
 				URI redirectedURIOrNull = getRelativeURI(URI.create(httpURLConnection.getHeaderField("Location")));
 				if (redirectedURIOrNull != null) {
+					System.err.println("  301 - URI " + target.toString()
+							+ " redirects to " + redirectedURIOrNull.toString());
 					discovered.add(redirectedURIOrNull);
 					_remappedURIs.put(uri, redirectedURIOrNull);
 					return discovered;
 				}
-			}
-			else if (responseCode >= 400) {
+			} else if (responseCode >= 400) {
+				System.err.println("  " + responseCode
+						+ " - An error occured resolving URI "
+						+ target.toString());
 				return discovered;
 			}
 		}
-		InputStream is = urlConnection.getInputStream();
-		Scanner s = new Scanner(is).useDelimiter("\\A");
-		String html = s.hasNext() ? s.next() : "";
+		String html = inputStreamToString(urlConnection.getInputStream());
 
 		Document document = Jsoup.parse(html);
 		Elements anchorLinks = document.select("a[href]");
 		for (Element el : anchorLinks) {
-			discovered.add(URI.create(el.attr("href")));
+			URI discoveredURI = URI.create(el.attr("href"));
+			System.err.println("  Discovered link to "
+					+ discoveredURI.toString());
+			discovered.add(discoveredURI);
 		}
-		is.close();
-		s.close();
 		return discovered;
 	}
 
@@ -87,9 +107,33 @@ public class WebTrawler {
 			String uriAsString = uri.toString();
 			if (uriAsString.startsWith(_rootURIAsString)) {
 				return URI.create(uriAsString.substring(_rootURIAsString.length()));
-			}
-			else {
+			} else {
 				return null;
+			}
+		}
+	}
+
+	/**
+	 * @return The input stream as a string or null if an exception occured.
+	 */
+	private String inputStreamToString(final InputStream is) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+			return builder.toString();
+		} catch (IOException e) {
+			// Probably bad
+			return null;
+		}
+		finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				// Swallow
 			}
 		}
 	}
